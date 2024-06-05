b29076ecbb687d283e39f66afb0b7319e87a7f2e
Serhii Kasianchuk <k.kasan2016@gmail.com>
2024-06-05 23:07:33 +0300

    create structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,46 @@
-import './App.css';
+import React from 'react';
+import "./App.css";
 
-function App() {
-  return <></>
+// components
+import Page from './../Page/Page';
+import Header from './../Header/Header';
+import Navigation from './../Navigation/Navigation';
+import Hero from './../Hero/Hero';
+import Logos from './../Logos/Logos';
+import Testimonial from './../Testimonial/Testimonial';
+import Features from './../Features/Features';
+import Video from './../Video/Video';
+import Pricing from './../Pricing/Pricing';
+import FAQ from './../FAQ/FAQ';
+import Accordion from './../Accordion/Accordion';
+import CTA from './../CTA/CTA';
+import Footer from './../Footer/Footer';
+
+
+
+const App = () => {
+  return (
+    <Page>
+      <Header>
+        <Navigation />
+        <Hero />
+      </Header>
+      <Logos />
+      <Testimonial></Testimonial>
+      <Features />
+      <Testimonial></Testimonial>
+      <Video />
+      <Testimonial></Testimonial>
+      <Pricing />
+      <Testimonial></Testimonial>
+      <FAQ>
+        <Accordion />
+      </FAQ>
+      <Testimonial></Testimonial>
+      <CTA />
+      <Footer />
+    </Page>
+  )
 }
 
 export default App
