2d88a1df0c2ab0964e94268d78a5a6633e7e96d4
Serhii Kasianchuk <k.kasan2016@gmail.com>
2024-06-14 07:55:19 +0300

    1

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,12 +3,14 @@ import "./Navigation.css";
 import logo from "../../assets/logo.svg"
 
 const Navigation = () => {
-  return <nav className="navigation">
+  return (
+  <nav className="navigation">
     <div className="navigation__logo-section">
-      <img className="navigation__logo" src={logo} alt="logo"/>
+      <img className="navigation__logo" src={logo} alt="logo" />
       <h3 className="navigation__name">SmartNotes</h3>
     </div>
   </nav>
-}
+  );
+};
 
 export default Navigation
