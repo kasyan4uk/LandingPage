186b9f464975079433cac194885ff9fd862b35ec
Serhii Kasianchuk <k.kasan2016@gmail.com>
2024-06-17 21:46:00 +0300

    update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import "./Hero.css";
+import arrow from "../../assets/arrow.svg";
+import abstractShapes from "../../assets/abstract-shapes.png"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="hero">
+      <div className="hero__column">
+        <h1 className="h1 hero__heading">
+          <span className="hero__header-gradient">Intelligent</span>
+          cloud-based{" "}
+          <span className="hero__header-gradient">note-taking</span>
+          and collaboration tool
+        </h1>
+        <p className="text-reg hero__subheading">Experience the power of smart note-taking and transform the way you work today.</p>
+        <div className="hero__input-container">
+          <input className="hero__input" type="email" placeholder="Enter your email"/>
+          <button className="text-reg hero__submit">
+            Sign up
+            <img className="hero__arrow" src={arrow} alt="arrow" />
+          </button>
+        </div>
+      </div>
+      <div className="hero__column">
+        <img className="hero__graphic" src={abstractShapes} alt="abstract shapes"/>
+      </div>
+    </section>
   )
 }
 
