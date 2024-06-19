718ef2452e9ef02c4c001dbe99e54d6b28cdb11d
Serhii Kasianchuk <k.kasan2016@gmail.com>
2024-06-19 17:45:15 +0300

    finish Testimonial section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import "./App.css";
 
+// fonts
 import "../../fonts/fonts.css"
+
+// img
+import doubleQuote from "../../assets/double-quote.svg"
 
 // components
 import Page from './../Page/Page';
@@ -28,7 +32,16 @@ const App = () => {
         <Hero />
       </Header>
       <Logos />
-      <Testimonial></Testimonial>
+      <Testimonial>
+        <h5 className="h5 testimonial__heading">
+          The real-time synchronization and AI-powered organization have made our workflow more efficient than ever.
+        </h5>
+        <div className="testimonial__author-section">
+          <p className="text-reg testimonial__author">Sarah Johnson</p>
+          <p className="text-reg testimonial__organization">TechSavvy Solutions</p>
+        </div>
+        <img className="testimonial__quotes" src={doubleQuote} alt="quote" />
+      </Testimonial>
       <Features />
       <Testimonial></Testimonial>
       <Video />
--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import "./Testimonial.css";
+import PropTypes from 'prop-types';
 
-const Testimonial = () => {
+const Testimonial = ({ children }) => {
   return (
-    <div>Testimonial</div>
+    <section className="testimonial">
+      <div className="testimonial__container">{children}</div>
+    </section>
   )
 }
 
 export default Testimonial
+
+Testimonial.propTypes = {
+  children: PropTypes.node.isRequired,
+};
