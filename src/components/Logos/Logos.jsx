55729d7e06e11f64c326ac39181442e4ccf6cf4a
Serhii Kasianchuk <k.kasan2016@gmail.com>
2024-06-19 07:53:21 +0300

    finish Logos section

--- src/components/Logos/Logos.jsx
+++ src/components/Logos/Logos.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import "./Logos.css";
+import { logos } from "../../utils/constants" 
 
 const Logos = () => {
   return (
-    <div>Logos</div>
+    <section className="logos">
+      <div className="logos__slide">
+        {logos.map((img, i) => (
+          <img className="logo" key={i} src={img} alt="logo" />
+        ))}
+        {logos.map((img, i) => (
+          <img className="logo" key={i} src={img} alt="logo" />
+        ))}
+      </div>
+      <div className="logos__overlay logos__overlay-left"></div>
+      <div className="logos__overlay logos__overlay-right"></div>
+    </section>
   )
 }
 
